825f99237c3b2087c05edaf8ae6090909b79df32
Sebastian J <jsebastiansteven@gmail.com>
2026-01-08 22:26:29 -0500

    Configuracion inicial para mostrar el mapa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'Asin-Map';
+export class AppComponent implements AfterViewInit, OnDestroy {
+  page = {
+    title: "AsinMap",
+
+  }
+
+  private map: L.Map | undefined;
+
+  ngAfterViewInit() {
+    // Es vital inicializarlo en AfterViewInit porque el div #map 
+    // debe existir en el DOM antes de que Leaflet intente usarlo.
+    this.map = L.map('map').setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(this.map);
+  }
+
+  ngOnDestroy() {
+    // Limpieza para evitar fugas de memoria
+    if (this.map) {
+      this.map.remove();
+    }
+  }
 }
