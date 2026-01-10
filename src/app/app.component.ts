a02be55864779d9a89938acc5bf1fb94b75e01e9
Sebastian J <jsebastiansteven@gmail.com>
2026-01-09 20:35:50 -0500

    Limpieza del código y adición de evento, el cual coloca un marcador cuando el usuario da click sobre el mapa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,28 +10,61 @@ import * as L from 'leaflet';
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements AfterViewInit, OnDestroy {
-  page = {
+  public page = {
     title: "AsinMap",
+    zoom: 16,
+    urlAttribution: "http://www.openstreetmap.org/copyright",
+    textAttribution: "OpenStreetMap",
 
+  }
+
+  private ubication = {
+    title: "Plaza de nariño",
+    text: "Interser",
+    centralCoordinate: [1.2147162, -77.2808453],
+    markedPoint: [1.2147162, -77.2808453],
+    latitude: 0,
+    Length: 0
   }
 
   private map: L.Map | undefined;
+  private lastMarker: L.Marker | undefined;
 
   ngAfterViewInit() {
-    // Es vital inicializarlo en AfterViewInit porque el div #map 
-    // debe existir en el DOM antes de que Leaflet intente usarlo.
-    this.map = L.map('map').setView([51.505, -0.09], 13);
+    this.map = L.map('map').setView([4.6440635, -74.1218681], this.page.zoom);
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 19,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      attribution: `&copy; <a href="${this.page.urlAttribution}">${this.page.textAttribution}</a>`
     }).addTo(this.map);
+
+    console.log(this.ubication.latitude, this.ubication.Length);
+
+    this.map.on('click', this.onMapClick);
   }
 
   ngOnDestroy() {
-    // Limpieza para evitar fugas de memoria
     if (this.map) {
       this.map.remove();
+    }
+  }
+
+  // Evento que detecta las coordenadas sobre el lugar en donde se dio click en el mapa
+  private onMapClick = (e: any) => {
+    const { lat, lng } = e.latlng;
+    this.setAMarker(lat, lng);
+  }
+
+  // Pone un manrcador sobre el mapa cuando el usuario da click izquierdo
+  private setAMarker(lat: number, lng: number) {
+    if (this.map) {
+      if (this.lastMarker) {
+        this.map.removeLayer(this.lastMarker);
+      }
+
+      this.lastMarker = L.marker([lat, lng])
+        .addTo(this.map)
+        .bindPopup(this.ubication.title);
     }
   }
 }
