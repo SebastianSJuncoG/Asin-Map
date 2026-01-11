4fd451da1c4e2c83507ae9991e251d30064a9789
Sebastian J <jsebastiansteven@gmail.com>
2026-01-10 20:45:33 -0500

    - Implementación de un icono para asignar como marcador. - Implementación de libreria para manerar las rutas sobre el mapa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { AfterViewInit, Component, OnDestroy } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import * as L from 'leaflet';
+import 'leaflet-routing-machine';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -20,25 +21,33 @@ export class AppComponent implements AfterViewInit, OnDestroy {
     alertError: false
   }
 
+  public origenDestino = {
+    placeHolderOrigin: "Origen ...",
+    placeHolderDestination: "Destino ..."
+  }
+
   public ubication = {
     inicialCoordinate: [4.6440635, -74.1218681],
-    centralCoordinate: [] as number[],
+    centralCoordinate: [4.6225848,-74.1502975] as number[],
     regularExpresion: /^-?\d+(\.\d+)?,\s?-?\d+(\.\d+)?$/,
     zoom: 19,
     title: "Plaza de nariño",
     text: "Plaza central",
     coordsNews: "",
     inputCoordinate: "",
+    inputOrigin: "",
+    inputDestination: ""
   }
 
   private map: L.Map | undefined;
   private lastMarker: L.Marker | undefined;
 
   ngAfterViewInit() {
-    this.map = L.map('map').setView([this.ubication.inicialCoordinate[0], this.ubication.inicialCoordinate[1]], this.ubication.zoom);
+    this.map = L.map('map', { attributionControl: false }).setView([this.ubication.inicialCoordinate[0], this.ubication.inicialCoordinate[1]], this.ubication.zoom);
 
     if (this.map) {
       this.initializeMap(this.ubication.inicialCoordinate[0], this.ubication.inicialCoordinate[1]);
+      this.generarRuta();
 
       this.map.on('click', this.onMapClick);
     }
@@ -52,21 +61,40 @@ export class AppComponent implements AfterViewInit, OnDestroy {
   public clearLastMarkers = () => {
     if (this.lastMarker && this.map) {
       this.map.removeLayer(this.lastMarker);
+      this.ubication.inicialCoordinate = [];
     }
   }
 
-  public formatCoords() {
-    if(this.ubication.inputCoordinate && this.ubication.regularExpresion.test(this.ubication.inputCoordinate)){
+  public foundCoords() {
+    this.ubication.centralCoordinate = this.formatCoords(this.ubication.inputCoordinate);
+    if (this.ubication.centralCoordinate.length > 0) {
       this.page.alertError = false;
-      this.ubication.centralCoordinate = this.ubication.inputCoordinate.split(",").map(c => parseFloat(c.trim()));
-
       this.centerMap(this.ubication.centralCoordinate[0], this.ubication.centralCoordinate[1]);
       this.setAMarker(this.ubication.centralCoordinate[0], this.ubication.centralCoordinate[1]);
 
-    }else{
+    } else {
       this.page.alertError = true;
 
     }
+  }
+
+  public deleteSearch() {
+    this.ubication.inputCoordinate = "";
+    this.page.alertError = false;
+  }
+
+  public generarRuta() {
+    const origen = L.latLng(4.644, -74.121);
+    const destino = L.latLng(this.ubication.centralCoordinate[0], this.ubication.centralCoordinate[1]);
+
+    (L as any).Routing.control({
+      waypoints: [origen, destino],
+      lineOptions: {
+        styles: [{ color: 'blue', weight: 4 }]
+      },
+      routeWhileDragging: true,
+      addWaypoints: false
+    }).addTo(this.map);
   }
 
   // Evento que detecta las coordenadas sobre el lugar en donde se dio click en el mapa
@@ -80,7 +108,7 @@ export class AppComponent implements AfterViewInit, OnDestroy {
     if (this.map) {
       this.clearLastMarkers();
 
-      this.lastMarker = L.marker([lat, lng])
+      this.lastMarker = L.marker([lat, lng], { icon: this.customIcon })
         .addTo(this.map)
         .bindPopup(this.ubication.title);
     }
@@ -90,7 +118,7 @@ export class AppComponent implements AfterViewInit, OnDestroy {
     if (this.map) {
       L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
-        attribution: `&copy; <a href="${lat}">${lng}</a>`
+        attribution: ''
       }).addTo(this.map);
     }
   }
@@ -101,9 +129,28 @@ export class AppComponent implements AfterViewInit, OnDestroy {
     }
   }
 
-  private centerMap(lat: number, lng: number) {
+  private centerMap(lat: number, lng: number, zoom: number = this.ubication.zoom) {
     if (this.map) {
-      this.map.setView([lat, lng], this.ubication.zoom);
+      this.map.setView([lat, lng], zoom);
+    }
+  }
+
+  private customIcon = L.icon({
+    iconUrl: 'icons/camion.png', // Ruta a tu imagen en la carpeta assets
+    shadowUrl: '', // Opcional: sombra del marcador
+
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+  });
+
+  private formatCoords(coords: string): number[] {
+    if (this.ubication.inputCoordinate && this.ubication.regularExpresion.test(this.ubication.inputCoordinate)) {
+      return this.ubication.inputCoordinate.split(",").map(c => parseFloat(c.trim()))
+
+    } else {
+      return [];
     }
   }
 }
